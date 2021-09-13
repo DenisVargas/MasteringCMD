ec175f64be1c32fc08737a92c8b1e00670bf2938
Denis Vargas Rivero [DV] <denis.vargasrivero@outlook.com>
2021-09-13 20:25:22 -0300

    Input and Ouput Update

--- IO.bat
+++ IO.bat
@@ -1,4 +1,25 @@
 :://////////////////////////////////////////////////////////////////////////////////////
+::                        -Standart IO-
+:://////////////////////////////////////////////////////////////////////////////////////
+::Hay 3 archivos universales para el input.
+::Standart in  --> stdin  - Input al programa/script -> index = 0
+::Standart out --> stdout - output al display        -> index = 1
+::Standart Err --> stderr - errores al display       -> index = 2
+
+::Redirecting Output(Stdout and Stderr)
+dir C:\ > list.txt
+::Si anteponemos el index al simbolo > podemos indicar cual archivo estandar queremos redirigir
+dir C:\ 2> list.txt
+::Podemos combinar ambos bufferes utilizando el operador &
+dir C:\ > lists.txt 2>&1
+::Es posible suprimir el output de un programa utilizando el pseudo archivo NUL
+dir C:\ > NUL
+
+::Para trabajar con Stdin hay que usar un workaround.
+::Redirigimos el stdin de la terminal llamado CON
+TYPE CON > lists.txt
+
+:://////////////////////////////////////////////////////////////////////////////////////
 ::Operaciones IO Estándar: Create - Read - Write- Delete - Move - Rename
 :://////////////////////////////////////////////////////////////////////////////////////
 
